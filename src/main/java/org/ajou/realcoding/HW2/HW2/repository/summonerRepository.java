824dd760a371e6d76f2193220a4e3f51d6fde2f9
rohguentak <rhgt78@gmail.com>
2019-07-11 23:00:08 +0900

    db에 저장까지

--- src/main/java/org/ajou/realcoding/HW2/HW2/repository/summonerRepository.java
+++ src/main/java/org/ajou/realcoding/HW2/HW2/repository/summonerRepository.java
@@ -1,15 +1,21 @@
 package org.ajou.realcoding.HW2.HW2.repository;
 
+import org.ajou.realcoding.HW2.HW2.domain.EncrypedInfo;
 import org.ajou.realcoding.HW2.HW2.domain.SummonerInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class summonerRepository {
+public class SummonerRepository {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    public void insertleagueposition(List<EncrypedInfo> encrypedInfo){
+        mongoTemplate.insert(encrypedInfo,EncrypedInfo.class);
+    }
 
    /* public static SummonerInfo findsummonerinfobyname(String summonerName) {
 
